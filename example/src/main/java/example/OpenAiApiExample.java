433b1cfb5d6dd6f306944ffc1b0c07e186e18af6
home1 <polischuk0107@gmail.com>
2023-04-15 19:20:04 +0300

    Changed example

--- example/src/main/java/example/OpenAiApiExample.java
+++ example/src/main/java/example/OpenAiApiExample.java
@@ -12,45 +12,52 @@ import java.util.HashMap;
 import java.util.List;
 
 class OpenAiApiExample {
-    public static void main(String... args) {
-        String token = System.getenv("OPENAI_TOKEN");
+
+    String prompt;
+
+    public OpenAiApiExample() {
+    }
+
+    public static void request(String token, String model, Integer maxToken, String prompt) {
+        //String token = System.getenv("OPENAI_TOKEN");
         OpenAiService service = new OpenAiService(token);
 
         System.out.println("\nCreating completion...");
         CompletionRequest completionRequest = CompletionRequest.builder()
-                .model("ada")
-                .prompt("Somebody once told me the world is gonna roll me")
+                .maxTokens(maxToken)
+                .model(model)
+                .prompt(prompt)
                 .echo(true)
                 .user("testing")
                 .n(3)
                 .build();
         service.createCompletion(completionRequest).getChoices().forEach(System.out::println);
-
-        System.out.println("\nCreating Image...");
-        CreateImageRequest request = CreateImageRequest.builder()
-                .prompt("A cow breakdancing with a turtle")
-                .build();
-
-        System.out.println("\nImage is located at:");
-        System.out.println(service.createImage(request).getData().get(0).getUrl());
-
-        System.out.println("Streaming chat completion...");
-        final List<ChatMessage> messages = new ArrayList<>();
-        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are a dog and will speak as such.");
-        messages.add(systemMessage);
-        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
-                .builder()
-                .model("gpt-3.5-turbo")
-                .messages(messages)
-                .n(1)
-                .maxTokens(50)
-                .logitBias(new HashMap<>())
-                .build();
-
-        service.streamChatCompletion(chatCompletionRequest)
-                .doOnError(Throwable::printStackTrace)
-                .blockingForEach(System.out::println);
-
-        service.shutdownExecutor();
+//
+//        System.out.println("\nCreating Image...");
+//        CreateImageRequest request = CreateImageRequest.builder()
+//                .prompt("A cow breakdancing with a turtle")
+//                .build();
+//
+//        System.out.println("\nImage is located at:");
+//        System.out.println(service.createImage(request).getData().get(0).getUrl());
+//
+//        System.out.println("Streaming chat completion...");
+//        final List<ChatMessage> messages = new ArrayList<>();
+//        final ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(), "You are a dog and will speak as such.");
+//        messages.add(systemMessage);
+//        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+//                .builder()
+//                .model("gpt-3.5-turbo")
+//                .messages(messages)
+//                .n(1)
+//                .maxTokens(50)
+//                .logitBias(new HashMap<>())
+//                .build();
+//
+//        service.streamChatCompletion(chatCompletionRequest)
+//                .doOnError(Throwable::printStackTrace)
+//                .blockingForEach(System.out::println);
+//
+//        service.shutdownExecutor();
     }
 }
